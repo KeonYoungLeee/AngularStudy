df19b30eeab67bce5a51c47b346ec678a3d3e738
KeonYoungLeee <ygy5036@gmail.com>
2020-08-27 17:47:32 +0900

    Add 강좌2(Angular Component 연결1-셋팅)

--- StudyProject/Front/src/app/app.module.ts
+++ StudyProject/Front/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Sample2Component } from './sample2/sample2.component';
 import { Sample3Component } from './sample3/sample3.component';
+import { GrandComponent } from './grand/grand.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Sample2Component,
-    Sample3Component
+    Sample3Component,
+    GrandComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
